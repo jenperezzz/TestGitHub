8e4b383b14df7dcc7487a0b922392452d63fbc74
Jen <46449946+jenperezzz@users.noreply.github.com>
2019-01-31 11:08:41 -0500

    Add files via upload

---
+++ HeresJohnny.java
@@ -0,0 +1,16 @@
+
+public class HeresJohnny {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int someNumber = 42;
+		int anotherNumber = someNumber + 5;
+		String someString;
+		String myName = "Johnny";
+		someString = "Here's " + myName;
+		System.out.print(someString);
+		
+
+	}
+
+}
